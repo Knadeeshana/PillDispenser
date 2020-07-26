65ac79a21e47ef0f2c335eca163c69bd6dc8b423
Kavindu Kumara <knadeeshana@gmail.com>
2020-07-26 09:40:08 +0530

    schedules on/off status

--- lib/Services.dart
+++ lib/Services.dart
@@ -1,36 +1,37 @@
 import 'dart:convert';
-import 'package:http/http.dart'as http;
+import 'package:http/http.dart' as http;
 
-
-
-
-Future<MedicinetableHome> fetchtable() async{
+Future<MedicinetableHome> fetchtable() async {
   var map = Map<String, dynamic>();
-  map['action']='Get_ALL';
+  map['action'] = 'Get_ALL';
   //final response =await http.post('http://192.168.137.1/phplessons/flutter.php',body: map);
   //print(response.body.toString());
-  String jsonresponse='[{"medicine": "amoxillin","numpills":4, "receivetime":"1 AM", "state":"true"},{"medicine": "amoxilin","numpills":3, "receivetime":"12 AM", "state":"false"},{"medicine": "Flagyl","numpills":1, "receivetime":"3 PM", "state":"false"}]';
-  final jsonResponse=json.decode(jsonresponse);
+  String jsonresponse =
+      '[{"medicine": "amoxillin","numpills":4, "receivetime":"1 AM", "state":"true"},{"medicine": "amoxilin","numpills":3, "receivetime":"12 AM", "state":"false"},{"medicine": "Flagyl","numpills":1, "receivetime":"3 PM", "state":"false"}]';
+  final jsonResponse = json.decode(jsonresponse);
   //print(jsonresponse);
-  MedicinetableHome album=new MedicinetableHome.fromJson(jsonResponse);
+  MedicinetableHome album = new MedicinetableHome.fromJson(jsonResponse);
   //print(album.hometable[1].medicine);
-  return(album);
-
+  return (album);
 }
 
-
-editPatient(String salutation,String firstName,String lastName){
-  Map<String,dynamic> map={'action':'Update_Patient','salutation':salutation,'firstName':firstName,'lastName':lastName};
+editPatient(String salutation, String firstName, String lastName) {
+  Map<String, dynamic> map = {
+    'action': 'Update_Patient',
+    'salutation': salutation,
+    'firstName': firstName,
+    'lastName': lastName
+  };
   //final response=await http.post('url',body:map);
-
 }
 
-Future<LoadedMedicationx> fetchMedications()async{
+Future<LoadedMedicationx> fetchMedications() async {
   var map = Map<String, dynamic>();
-  map['action']='Get_Medication';
-  String _jsonresponse='[{"medicine": "Amoxillin","dose strength":200, "Schedules":[{"time":"9.00 AM","pills":1},{"time":"5.00PM","pills":2}]},{"medicine": "Flagyl","dose strength":500, "Schedules":[{"time":"10.00 AM","pills":2},{"time":"6.00PM","pills":2}]}]';
-  final jsonresponse=json.decode(_jsonresponse);
-  LoadedMedicationx medications=LoadedMedicationx.fromJson(jsonresponse);
+  map['action'] = 'Get_Medication';
+  String _jsonresponse =
+      '[{"medicine": "Amoxillin","dose strength":200, "Schedules":[{"time":"9.00 AM","pills":1},{"time":"5.00PM","pills":2},{"time":"10.00PM","pills":2}]},{"medicine": "Flagyl","dose strength":500, "Schedules":[{"time":"10.00 AM","pills":2},{"time":"6.00PM","pills":2}]}]';
+  final jsonresponse = json.decode(_jsonresponse);
+  LoadedMedicationx medications = LoadedMedicationx.fromJson(jsonresponse);
   /*print(medications.toString());
   print(medications.loadedMedication.toString());
   print(medications.loadedMedication[0].medicine);
@@ -86,18 +87,15 @@ class Schedules {
   }
 }
 
-
-class LoadedMedicationx{
+class LoadedMedicationx {
   final List<LoadedMedication> loadedMedication;
   LoadedMedicationx({this.loadedMedication});
 
-  factory LoadedMedicationx.fromJson(List<dynamic>json){
-    List<LoadedMedication> _templist= new List<LoadedMedication>();
-    _templist=json.map((f)=>LoadedMedication.fromJson(f)).toList();
-    return LoadedMedicationx(
-      loadedMedication: _templist);
+  factory LoadedMedicationx.fromJson(List<dynamic> json) {
+    List<LoadedMedication> _templist = new List<LoadedMedication>();
+    _templist = json.map((f) => LoadedMedication.fromJson(f)).toList();
+    return LoadedMedicationx(loadedMedication: _templist);
   }
-
 }
 /*
 class Medicine{
@@ -147,45 +145,31 @@ class Schedules{
   }
 }*/
 
-
-
-
-
-
-
-
-
-
-
-class MedicinetableHome{
+class MedicinetableHome {
   final List<Medicineloader> hometable;
 
   MedicinetableHome({this.hometable});
 
-  factory MedicinetableHome.fromJson(List<dynamic>jsons){
-    List <Medicineloader>_temptable = new List<Medicineloader>();
-    _temptable=jsons.map((i)=>Medicineloader.fromJson(i)).toList();
-    return MedicinetableHome(
-      hometable: _temptable);
+  factory MedicinetableHome.fromJson(List<dynamic> jsons) {
+    List<Medicineloader> _temptable = new List<Medicineloader>();
+    _temptable = jsons.map((i) => Medicineloader.fromJson(i)).toList();
+    return MedicinetableHome(hometable: _temptable);
   }
-
 }
 
-class Medicineloader{
+class Medicineloader {
   final String medicine;
   final String numpills;
   final String receivetime;
   final String state;
-  
-  Medicineloader({this.medicine,this.numpills,this.receivetime,this.state});
 
-  factory Medicineloader.fromJson(Map<String,dynamic>json){
+  Medicineloader({this.medicine, this.numpills, this.receivetime, this.state});
+
+  factory Medicineloader.fromJson(Map<String, dynamic> json) {
     return Medicineloader(
-      medicine:json['medicine'],
-      numpills: json['numpills'].toString(),
-      receivetime: json['receivetime'],
-      state: (json['state']=='true')?'taken':'not taken'
-
-    );
+        medicine: json['medicine'],
+        numpills: json['numpills'].toString(),
+        receivetime: json['receivetime'],
+        state: (json['state'] == 'true') ? 'taken' : 'not taken');
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,25 +7,25 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:pill_dispensor/NavigatorPages/Settings/EditPatient.dart';
 import 'package:pill_dispensor/NavigatorPages/Medications/AddMedicine.dart';
 import 'package:pill_dispensor/NavigatorPages/Medications/medications.dart';
+import 'package:pill_dispensor/NavigatorPages/Medications/EditMedicine.dart';
 import 'package:pill_dispensor/Login/login.dart';
 
-
 void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-      //home:Login(),
+      debugShowCheckedModeBanner: false,
+      home: Login(),
       initialRoute: '/navigator',
       routes: {
-        '/login':(context)=>Login(),
-        '/signUp':(context)=> SignUp(),
+        '/login': (context) => Login(),
+        '/signUp': (context) => SignUp(),
         '/navigator': (context) => MyBottomNavigationBar(),
         '/home': (context) => new Home(),
         '/settings': (context) => new Settings(),
         '/EditPatient': (context) => new EditPatient(),
-        '/AddMedicine':(context)=> new AddMedicine(),
-        '/medications':(context)=>Medications()
+        '/AddMedicine': (context) => new AddMedicine(),
+        '/medications': (context) => Medications(),
+        '/EditMedicine': (context) => EditMedicine()
       },
     ));
-
 
 class MyBottomNavigationBar extends StatefulWidget {
   @override
@@ -33,52 +33,55 @@ class MyBottomNavigationBar extends StatefulWidget {
 }
 
 class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
-  int _currentIndex=3;
-  final List<Widget> _children=[Settings(),null,Medications(),Home()]; //bottom navigation bar calls
-
+  int _currentIndex = 3;
+  final List<Widget> _children = [
+    Settings(),
+    null,
+    Medications(),
+    Home()
+  ]; //bottom navigation bar calls
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:_children[_currentIndex], //set to index 3 initially to show Home page
+      body:
+          _children[_currentIndex], //set to index 3 initially to show Home page
       bottomNavigationBar: CurvedNavigationBar(
-          backgroundColor: Colors.white,
-          index: 3, //initial index
-          color: Colors.teal,
-          buttonBackgroundColor: Colors.brown[700],
-          height: 60,
-          items: <Widget>[
+        backgroundColor: Colors.white,
+        index: 3, //initial index
+        color: Colors.teal,
+        buttonBackgroundColor: Colors.brown[700],
+        height: 60,
+        items: <Widget>[
+          Icon(
+            Icons.settings,
+            size: 30,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.notification_important,
+            size: 30,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.bubble_chart,
+            size: 30,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.home,
+            size: 40,
+            color: Colors.white,
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
 
-            Icon(
-              Icons.settings,
-              size: 30,
-              color: Colors.white,
-            ),
-            Icon(
-              Icons.notification_important,
-              size: 30,
-              color: Colors.white,
-            ),
-            Icon(
-              Icons.bubble_chart,
-              size: 30,
-              color: Colors.white,
-            ),
-            Icon(
-              Icons.home,
-              size: 40,
-              color: Colors.white,
-            ),
-          ],
-          onTap: (index) {
-            setState(() {
-              _currentIndex=index;
-            });
-            
-            //Handle button tap
-          },
-        ),
-      
+          //Handle button tap
+        },
+      ),
     );
   }
 }
