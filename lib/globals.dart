7412d13804b7d10bc9b7b8572aae655c09f8f6b3
Kavindu Kumara <knadeeshana@gmail.com>
2021-01-07 22:45:13 +0530

    global variable document added

---
+++ lib/globals.dart
@@ -0,0 +1,8 @@
+import 'package:pill_dispensor/Services/Services.dart';
+
+List<Compartment> compartments = [];
+String scheduleState;
+String deviceID;
+String liquidMedicine;
+List<String> pillMedicine;
+Future<LoadedMedication> medicationtable;
